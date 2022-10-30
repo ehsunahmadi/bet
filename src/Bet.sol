7410966633e2c440efb78e375366cbcc0dbd33a6
ehsan <ehsun.ahmadi@gmail.com>
2022-10-30 20:16:44 +0330

    initialize after charging house

--- src/Bet.sol
+++ src/Bet.sol
@@ -3,31 +3,34 @@
 pragma solidity 0.8.16;
 
 struct Pick {
-  uint8 line;
+  uint256 line;
   string name;
 }
 
 struct SingleBet {
   address payable bettorAddress;
-  uint8 bettorLine;
+  uint256 line;
   uint256 amount;
 }
 
-error Bet__SendMoreToBet(unit256 indexed amount);
-error Bet__SendLessToBet(unit256 indexed amount);
+error Bet__SendMoreToBet(uint256 amount);
+error Bet__SendLessToBet(uint256 amount);
+error Bet__ContracNotInitialized();
 
 
 contract Bet {
-  uint8 public constant MINIMUM_BET = 0.005 ether;
+  uint256 public constant MINIMUM_BET = 0.005 ether;
   // uint8 public constant FEE_PERCENTAGE = uint8(1) / uint8(10);
 
   uint256 public immutable i_startUpdating;
   uint256 public immutable i_interval;
-  uint256 private s_lastTimeStamp;
-
+  bool public  i_initizialized = false;
   Pick public i_favorite;
   Pick public i_dog;
+  
 
+
+  uint256 private s_lastTimeStamp;
   SingleBet[] private s_favoritters;
   SingleBet[] private s_doggers;
 
@@ -47,23 +50,28 @@ contract Bet {
   }
 
   function bet(bool wantsFavorite) external payable {
+
+    if (this.i_initizialized) {
+      revert Bet__ContracNotInitialized();
+    }
+    
     if (msg.value < MINIMUM_BET) {
       revert Bet__SendMoreToBet(this.MINIMUM_BET);
     }
-
-    if (msg.value > ((this.getBalacnce()*100)/(wantsFavorite ? i_favorite.line : i_dog.line))) {
-        revert Bet__SendLessToBet();
+    uint256 max = (this.getBalacnce()*100)/(wantsFavorite ? i_favorite.line : i_dog.line);
+    if (msg.value > max) {
+        revert Bet__SendLessToBet(max);
     }
 
+    uint256 newB = msg.value + this.getBalacnce();
+    uint256 oldFavPerc = (i_favorite.line/100)* this.getBalacnce();
     if (wantsFavorite) {
-      s_favoritters.push(SingleBet(payable(msg.sender), i_favorite.line));
-      uint8 oldFavPerc = ((i_favorite.line/100)* this.getBalacnce();
-      newB = message.value + this.getBalacnce();
-      i_favorite.line = ((p * newB) / this.getBalacnce()) * 100;
+      s_favoritters.push(SingleBet(payable(msg.sender), i_favorite.line, msg.value));
+      i_favorite.line = ((oldFavPerc * newB) / this.getBalacnce()) * 100;
       i_dog.line = 100 - i_favorite.line;
     } else {
-      s_doggers.push(SingleBet(payable(msg.sender), i_dog.line));
-      i_dog.line = ((p * newB) / this.getBalacnce()) * 100;
+      s_doggers.push(SingleBet(payable(msg.sender), i_dog.line, msg.value));
+      i_dog.line = ((oldFavPerc * newB) / this.getBalacnce()) * 100;
       i_favorite.line = 100 - i_dog.line;
     }
 
@@ -71,13 +79,16 @@ contract Bet {
   }
 
   function getBalacnce() external view returns (uint8) {
-    adress(this).balance;
+    address(this).balance;
   }
 
   function chargeHouse() external payable {
-      favePerc uint8 = i_favorite.line / 100
+      uint256 favePerc = i_favorite.line / 100;
       s_favoritters.push(SingleBet(payable(msg.sender), i_favorite.line, favePerc * msg.value));
-      s_doggers.push(SingleBet(payable(msg.sender), (100 - favePerc) * msg.value));
+      s_doggers.push(SingleBet(payable(msg.sender), i_dog.line, (100 - favePerc) * msg.value));
+      if (!this.i_initizialized) {
+        this.i_initizialized = true;
+      } 
   }
 
 }
